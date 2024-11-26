84ae0126cd4d3465b5fe8b8cfe05b199848608fa
kvh <101593990+kvh03@users.noreply.github.com>
2024-11-26 11:29:42 +0530

    Update home.js

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -35,7 +35,7 @@ const Home = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('http://localhost:4000/predict-heart-disease', formData);
+            const response = await axios.post(`https://heartdisease-prediction.vercel.app/', formData);
             setPrediction(response.data.prediction);
         } catch (error) {
             console.error('Error fetching prediction:', error.response ? error.response.data : error.message);
