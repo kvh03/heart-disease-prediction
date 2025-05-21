6dde2aa4bf32dc7547c3828c9396b55212d36eef
kvh <101593990+kvh03@users.noreply.github.com>
2025-05-21 19:10:29 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
@@ -60,4 +61,5 @@ def index():
     return "Heart Disease Prediction API is running."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
